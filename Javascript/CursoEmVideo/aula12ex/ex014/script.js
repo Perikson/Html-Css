0eb0ae1510cc7d4bc928c7b8f4a1dfafa02864d1
Perikson <darcperikson@msn.com>
2022-10-01 23:33:37 -0300

    hora do dia

--- Javascript/CursoEmVideo/aula12ex/ex014/script.js
+++ Javascript/CursoEmVideo/aula12ex/ex014/script.js
@@ -1,6 +1,7 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+    var saud = window.document.getElementById('saudacao')
     var data = new Date()
     var hora = data.getHours()
     var minutos = data.getMinutes()
@@ -8,16 +9,22 @@ function carregar() {
     if (hora >= 6 && hora < 12) {
         // Bom dia
         img.src = `Imagens/manha.jpg`
-        document.body.section.style.background = '#4BDFFA'
+        document.body.style.background = '#65E4F5'
+        saud.innerText = 'BOM DIA!'
     } else if (hora >= 12 && hora < 18) {
         // Boa tarde.
         img.src = `Imagens/tarde.jpg`
-        document.body.style.background = '#4BDFFA'
+        document.body.style.background = '#E67A00'
+        saud.innerText = 'BOA TARDE!'
     } else if (hora >= 18 && hora <= 22) {
         // Boa Noite.
         img.src = `Imagens/noite1.jpeg`
+        saud.innerText = 'BOA NOITE!'
+        document.body.style.background = 'rgb(92, 88, 88)'
     } else {
         // Boa Madrugada.
         img.src = `Imagens/madrugada.jpeg`
+        saud.innerText = 'BOA MADRUGADA!'
+        document.body.style.background = 'black'
     }
 }
